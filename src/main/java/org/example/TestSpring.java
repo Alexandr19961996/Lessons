71005262035e9b393079c1b06e7b5a0ca05d0542
Alexandr Borovik <www.sanya.boriy@mail.ru>
2023-12-24 22:28:32 +0300

    Scope (singleton and prototype)

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -5,10 +5,22 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        MusicPlayer firstmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        //сравнение двух бинов по указателю
+        System.out.println(firstmusicPlayer == secondmusicPlayer);
+        //вывод хэш-значений бина "firstmusicPlayer"
+        System.out.println(firstmusicPlayer);
+        //вывод хэш-значений бина "secondmusicPlayer"
+        System.out.println(secondmusicPlayer);
+
+        //изменение значений громкости у первого бина и проверка прохода этого изменения и у второго бина
+        firstmusicPlayer.setVolume(10);
+
+        System.out.println("firstmusicPlayer: " + firstmusicPlayer.getVolume());
+        System.out.println("secondmusicPlayer: " + secondmusicPlayer.getVolume());
+
         context.close();
     }
 }
