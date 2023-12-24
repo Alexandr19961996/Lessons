c391f93c57479e04b8a7fb77c3c6389ca4d947ee
Alexandr Borovik <www.sanya.boriy@mail.ru>
2023-12-24 23:59:01 +0300

    Init-method, destroy-method and factory-method

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,8 +1,24 @@
 package org.example;
 
 public class ClassicalMusic implements Music {
+
+    private ClassicalMusic() {
+    }
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
+    }
+
+    public void initMethod() {
+        System.out.println("Doing my initialisation");
+    }
+
+    public void destroyMethod() {
+        System.out.println("Doing my destruction");
     }
 }
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -5,21 +5,26 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer firstmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        ClassicalMusic firstclassicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        ClassicalMusic secondclassicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(firstclassicalMusic);
+        System.out.println(secondclassicalMusic);
 
-        //сравнение двух бинов по указателю
-        System.out.println(firstmusicPlayer == secondmusicPlayer);
-        //вывод хэш-значений бина "firstmusicPlayer"
-        System.out.println(firstmusicPlayer);
-        //вывод хэш-значений бина "secondmusicPlayer"
-        System.out.println(secondmusicPlayer);
-
-        //изменение значений громкости у первого бина и проверка прохода этого изменения и у второго бина
-        firstmusicPlayer.setVolume(10);
-
-        System.out.println("firstmusicPlayer: " + firstmusicPlayer.getVolume());
-        System.out.println("secondmusicPlayer: " + secondmusicPlayer.getVolume());
+//        MusicPlayer firstmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        //сравнение двух бинов по указателю
+//        System.out.println(firstmusicPlayer == secondmusicPlayer);
+//        //вывод хэш-значений бина "firstmusicPlayer"
+//        System.out.println(firstmusicPlayer);
+//        //вывод хэш-значений бина "secondmusicPlayer"
+//        System.out.println(secondmusicPlayer);
+//
+//        //изменение значений громкости у первого бина и проверка прохода этого изменения и у второго бина
+//        firstmusicPlayer.setVolume(10);
+//
+//        System.out.println("firstmusicPlayer: " + firstmusicPlayer.getVolume());
+//        System.out.println("secondmusicPlayer: " + secondmusicPlayer.getVolume());
 
         context.close();
     }
