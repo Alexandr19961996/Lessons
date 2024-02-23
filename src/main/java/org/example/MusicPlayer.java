84216a1e48a572f76bb9ce5e3ad04b9ed7e6b52d
Alexandr Borovik <www.sanya.boriy@mail.ru>
2024-02-23 16:08:01 +0300

    Summary

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,15 +1,20 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
+
+    // Эти 2 поля необходимы для демонстрации внедрения простых значений в bean в AC.
     private Music music;
     private String name;
     private int volume;
+    private List<Music> musicList = new ArrayList<>();
 
+    // Пустой конструктор необходим, если происходит Dependency Injection через setter.
     public MusicPlayer() {
-
     }
 
-    //IoC
     public MusicPlayer(Music music) {
         this.music = music;
     }
@@ -34,7 +39,23 @@ public class MusicPlayer {
         return volume;
     }
 
+    public List<Music> getMusicList() {
+        return musicList;
+    }
+
+    // Реализация задания: создание списка музыкальных песен разных жанров, внедрение зависимостей через
+    // AC, запуск их в музыкальном плеере
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
     public void playMusic() {
+        // Вывод одиночной песни.
         System.out.println("Playing: " + music.getSong());
+
+//        // Вывод списка песен.
+//        for (int i = 0; i < musicList.size(); i++) {
+//            System.out.println(musicList.get(i).getSong());
+//        }
     }
 }
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -5,26 +5,60 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ClassicalMusic firstclassicalMusic = context.getBean("musicBean", ClassicalMusic.class);
-        ClassicalMusic secondclassicalMusic = context.getBean("musicBean", ClassicalMusic.class);
-        System.out.println(firstclassicalMusic);
-        System.out.println(secondclassicalMusic);
 
-//        MusicPlayer firstmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        MusicPlayer secondmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        // Тест и проверка работоспособности beans в Application Context.
+//        TestBean testBean = context.getBean("testBean", TestBean.class);
+//        System.out.println(testBean.getName());
+
+        // Программирование на уровне интерфейсов. Вызов bean, который в реальном коде
+        // implements интерфейс. Можно путем изменения в AC классов, которые implements данный интерфейс,
+        // добиться изменения работы логики программы.
+//        Music music = context.getBean("musicBean", Music.class);
+//        System.out.println(music.getSong());
+
+        //IoC.
+//        Music music = context.getBean("musicBean", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        musicPlayer.playMusic();
+
+//        // Dependency Injection.
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+
+//        // Вывод простых значений.
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
+
+        // Реализация задания со списком.
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+
+        // Демонстрация работы scope (Singleton и Prototype).
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 //
-//        //сравнение двух бинов по указателю
-//        System.out.println(firstmusicPlayer == secondmusicPlayer);
-//        //вывод хэш-значений бина "firstmusicPlayer"
-//        System.out.println(firstmusicPlayer);
-//        //вывод хэш-значений бина "secondmusicPlayer"
-//        System.out.println(secondmusicPlayer);
+//        // Сравнение двух Beans по указателю (на один ли участок памяти они указывают).
+//        System.out.println(firstMusicPlayer == secondMusicPlayer);
+//        // Вывод хэш-значений (поскольку в классе MusicPlayer не переопределен метод toString) Bean "firstMusicPlayer".
+//        System.out.println(firstMusicPlayer);
+//        // Вывод хэш-значений (поскольку в классе MusicPlayer не переопределен метод toString) Bean "secondMusicPlayer".
+//        System.out.println(secondMusicPlayer);
 //
-//        //изменение значений громкости у первого бина и проверка прохода этого изменения и у второго бина
-//        firstmusicPlayer.setVolume(10);
+//        // Изменение значений громкости у первого Bean и проверка прохода этого изменения у второго Bean
+//        firstMusicPlayer.setVolume(10);
 //
-//        System.out.println("firstmusicPlayer: " + firstmusicPlayer.getVolume());
-//        System.out.println("secondmusicPlayer: " + secondmusicPlayer.getVolume());
+//        System.out.println("firstMusicPlayer: " + firstMusicPlayer.getVolume());
+//        System.out.println("secondMusicPlayer: " + secondMusicPlayer.getVolume());
+
+        // Демонстрация вызова init и destroy methods. Для beans со scope = prototype не вызывается init-method.
+        // При наличии factory-method и scope = singleton создание объекта будет произведено один раз.
+        ClassicalMusic classicalMusicOne = context.getBean("musicBean", ClassicalMusic.class);
+        ClassicalMusic classicalMusicTwo = context.getBean("musicBean", ClassicalMusic.class);
+
+
+        System.out.println(classicalMusicOne.getSong());
+        System.out.println(classicalMusicTwo.getSong());
 
         context.close();
     }
