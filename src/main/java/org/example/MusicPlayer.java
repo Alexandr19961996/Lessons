a3fe9e8b027d110626092a4eac9d7ec76facebe1
Alexandr Borovik <www.sanya.boriy@mail.ru>
2024-02-26 22:59:24 +0300

    Annotations. @Qualifier

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -9,16 +10,19 @@ import java.util.List;
 @Component
 public class MusicPlayer {
 
-//    private Music music;
+    // Демонстрация "уточнения" зависимости через аннотацию @Qualifier. Вариант реализации через поле.
+    @Autowired
+    @Qualifier("classicalMusicBean")
+    private Music music;
 
     // Здесь производится тестирование работы аннотации @Autowiring (для этого создаем дополнительное поле, чтобы
     // исключить ситуации, когда под внедрение попадают сразу несколько Beans) Два поля используются, если происходит
     // внедрение зависимостей сразу у двух Beans, имплементирующих интерфейс.
 
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+//    private ClassicalMusic classicalMusic;
+//    private RockMusic rockMusic;
 
-    // Эти 2 поля необходимы для демонстрации внедрения простых значений в bean в AC.
+    // Эти 2 поля необходимы для демонстрации внедрения простых значений в Bean в AC.
     private String name;
     private int volume;
 
@@ -28,8 +32,9 @@ public class MusicPlayer {
 //    public MusicPlayer() {
 //    }
 
+    // Демонстрация "уточнения" зависимости через аннотацию @Qualifier. Вариант реализации через конструктор.
 //    @Autowired
-//    public MusicPlayer(Music music) {
+//    public MusicPlayer(@Qualifier("classicalMusicBean") Music music) {
 //        this.music = music;
 //    }
 
@@ -43,10 +48,11 @@ public class MusicPlayer {
 //        this.music = music;
 //    }
 
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-    }
+//    @Autowired
+//    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+//        this.classicalMusic = classicalMusic;
+//        this.rockMusic = rockMusic;
+//    }
 
     public void setName(String name) {
         this.name = name;
@@ -69,16 +75,16 @@ public class MusicPlayer {
     }
 
     // Реализация задания: создание списка музыкальных песен разных жанров, внедрение зависимостей через
-    // AC, запуск их в музыкальном плеере
+    // AC, запуск их в музыкальном плеере.
     public void setMusicList(List<Music> musicList) {
         this.musicList = musicList;
     }
 
     public void playMusic() {
 //        // Вывод одиночной песни.
-//        System.out.println("Playing: " + music.getSong());
+        System.out.println("Playing: " + music.getSong());
 //        System.out.println("Playing: " + classicalMusic.getSong());
-        System.out.println("Playing: " + classicalMusic.getSong() + " + " + rockMusic.getSong());
+//        System.out.println("Playing: " + classicalMusic.getSong() + " + " + rockMusic.getSong());
 
 //        // Вывод списка песен.
 //        for (int i = 0; i < musicList.size(); i++) {
